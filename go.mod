6953471350cf1ce8a508eddcca8e97030886594e
Tim Chernykh <tim.cherry.co@gmail.com>
2021-08-13 22:31:39 +0300

    google-api: i've copied example code from google

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module unicorn.dev.web-scrap
 
 go 1.16
+
+require (
+	cloud.google.com/go v0.91.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/serpapi/google-search-results-golang v0.0.0-20200815030216-632c97dac1ab
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.54.0
+	google.golang.org/genproto v0.0.0-20210813162853-db860fec028c // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"sync"
-	ctxParser "unicorn.dev.web-scrap/BlackBox"
-	webScrapper "unicorn.dev.web-scrap/MagicBox"
+	magicBox "unicorn.dev.web-scrap/MagicBox"
 )
 
 func main() {
-	startScrapper := flag.Bool("scrapper", false, "Start web scrapper process")
-	startParser := flag.Bool("parser", false, "Start web parser process")
 
-	flag.Parse()
+	magicBox.ReqString("qwe",
+		[]string{
+			"Moscow",
+		})
+	//startScrapper := flag.Bool("scrapper", false, "Start web scrapper process")
+	//startParser := flag.Bool("parser", false, "Start web parser process")
 
-	if !*startScrapper && !*startParser {
-		fmt.Println("Please, choose at least one of mode")
-		os.Exit(1)
-	}
+	//flag.Parse()
+	//
+	//if !*startScrapper && !*startParser {
+	//	fmt.Println("Please, choose at least one of mode")
+	//	os.Exit(1)
+	//}
+	//
+	//var wg sync.WaitGroup
+	//
+	//if *startScrapper {
+	//	wg.Add(1)
+	//	go webScrapper.StartScrapper(&wg)
+	//}
+	//
+	//if *startParser {
+	//	wg.Add(1)
+	//	go ctxParser.StartParser(&wg)
+	//
+	//}
 
-	var wg sync.WaitGroup
-
-	if *startScrapper {
-		wg.Add(1)
-		go webScrapper.StartScrapper(&wg)
-	}
-
-	if *startParser {
-		wg.Add(1)
-		go ctxParser.StartParser(&wg)
-
-	}
-
-	wg.Wait()
+	//wg.Wait()
 }
